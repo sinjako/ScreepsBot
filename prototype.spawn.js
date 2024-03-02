450b401a561aa91514da6a9ce8207a078d2f02ce
Haider al Saadi <sinjako123@hotmail.com>
2024-03-03 00:33:04 +0100

    removed some logging

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -8,14 +8,12 @@ StructureSpawn.prototype.spawnCreepsIfNecessary =
         // find all creeps in room
         /** @type {Array.<Creep>} */
         let creepsInRoom = room.find(FIND_MY_CREEPS);
-        console.log(creepsInRoom)
         // count the number of creeps alive for each role in this room
         // _.sum will count the number of properties in Game.creeps filtered by the
         //  arrow function, which checks for the creep being a specific role
         /** @type {Object.<string, number>} */
         let numberOfCreeps = {};
         for (let role of listOfRoles) {
-            console.log(numberOfCreeps)
             numberOfCreeps[role] = _.sum(creepsInRoom, (c) => c.memory.role == role);
         }
         let maxEnergy = room.energyCapacityAvailable;
